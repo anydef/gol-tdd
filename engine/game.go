b37eeeb8ff0167cdfc96ae4a5d26c9b94c43c45f
pavlo.fedyna <fed.pavlo@gmail.com>
2017-12-02 21:51:29 +0100

    Introduced cell

--- engine/game.go
+++ engine/game.go
@@ -1,1 +1,16 @@
 package engine
+
+type State int
+
+const (
+	Alive State = iota
+	Dead  State = iota
+)
+
+type Cell struct {
+	State State
+}
+
+func NewCell(s State) Cell {
+	return Cell{s}
+}
--- engine/game_test.go
+++ engine/game_test.go
@@ -2,10 +2,26 @@ package engine_test
 
 import (
 	//"testing"
-	//"github.com/anydef/gol-tdd/engine"
+	"github.com/anydef/gol-tdd/engine"
+	"testing"
 )
 
 const succeed = "\u2713"
 const fail = "\u2717"
 
+func TestCell_HasStatus(t *testing.T) {
+	var cell engine.Cell
 
+	cell = engine.NewCell(engine.Alive)
+
+	if cell.State != engine.Alive {
+		t.Fatalf("%s\t Cell is not Alive", fail)
+	}
+
+	cell = engine.NewCell(engine.Dead)
+
+	if cell.State != engine.Dead {
+		t.Fatalf("%s\t Cell is not Alive", fail)
+	}
+
+}
