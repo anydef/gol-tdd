d9ed4daeeee3d649a9c88317f5571c3626a761cd
pavlo.fedyna <fed.pavlo@gmail.com>
2017-12-03 17:54:55 +0100

    Block colony test

--- engine/game_rules_test.go
+++ engine/game_rules_test.go
@@ -38,26 +38,3 @@ func TestRule_Cell_with_1_neighbour_dies(t *testing.T) {
 	}
 
 }
-
-func TestRule_Cell_with_2_neighbours_stays_to_live(t *testing.T) {
-	var game engine.Game = engine.NewGame(10)
-	game.Seed(engine.NewCoordinate(1, 1), engine.Alive)
-	game.Seed(engine.NewCoordinate(1, 2), engine.Alive)
-	game.Seed(engine.NewCoordinate(2, 1), engine.Alive)
-
-	//grid := game.Next()
-
-	//t.Fatalf("Not implemented")
-	//if grid.CellAt(engine.NewCoordinate(1, 1)) != engine.Alive {
-	//	t.Fatalf("Single cell should die")
-	//}
-	//
-	//if grid.CellAt(engine.NewCoordinate(1, 2)) != engine.Alive {
-	//	t.Fatalf("Single cell should die")
-	//}
-	//
-	//if grid.CellAt(engine.NewCoordinate(2, 1)) != engine.Alive {
-	//	t.Fatalf("Single cell should die")
-	//}
-
-}
